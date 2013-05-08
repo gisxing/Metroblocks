39c38099671e669910a32a4be51ae50e1811c42e
Andrew Quintanilla <quint.andrew@gmail.com>
2013-05-08 16:20:07 -0700

    Added an offset to everything
    Allows for more flexible positioning of the grid on the screen. Still
    working on the falling blocks.

--- blocks.py
+++ blocks.py
@@ -13,17 +13,46 @@ tilesize = (45,45)
 liningsize = (tilesize[0]-4,tilesize[1]-4)
 innersize = (liningsize[0]-4,liningsize[1]-4)
 
+class BlockManager(pygame.sprite.Group):
+    def __init__(self, color1, color2, wiperspeed, dropspeed, xoffset=0,
+                 yoffset=0):
+        pygame.sprite.Group.__init__(self)
+        self.color1 = color1
+        self.color2 = color2
+        self.waitingcount = -1
+        self.fallingblock = False
+        self.grid = [[0 for y in range(10)] for x in range(16)]
+        self.xoffset = xoffset
+        self.yoffset = yoffset
+        self.wiper = Wiper(wiperspeed,self.xoffset,self.yoffset)
+        self.dropspeed = dropspeed
+        self.grid = Gridlines(self.xoffset,self.yoffset)
+
+    def update(self, time):
+        self.wiper.update(time)
+        if (self.waitingblock == -1):
+            
+        else:
+            self.block = Block(self.color1,self.color2)
+            self.add(self.block)
+            self.fallingblock = False
+
+    def draw(self, Surface):
+        self.grid.draw(Surface)
+        self.wiper.draw(Surface)
+
 class Tile(pygame.sprite.Sprite):
     def __init__(self, color1, color2):
         pygame.sprite.Sprite.__init__(self)
+        self.border = pygame.Surface
         self.image = pygame.Surface(tilesize)
         self.image.fill(color1)
-        lining = pygame.Surface(liningsize)
-        lining.fill(color2)
-        inner = pygame.Surface(innersize)
-        inner.fill(color1)
-        self.image.blit(lining,(2,2))
-        self.image.blit(inner,(4,4))
+        self.lining = pygame.Surface(liningsize)
+        self.lining.fill(color2)
+        self.inner = pygame.Surface(innersize)
+        self.inner.fill(color1)
+        self.image.blit(self.lining,(2,2))
+        self.image.blit(self.inner,(4,4))
 
 # Returns a random block
 class Block(pygame.sprite.Group):
@@ -51,8 +80,6 @@ class Block(pygame.sprite.Group):
         x3 = self.layout[3]
         self.layout = (x1,x2,x3,x0)
 
-    #def update(self):
-
 class Line(pygame.sprite.Sprite):
     def __init__(self, size, pos):
         pygame.sprite.Sprite.__init__(self)
@@ -62,30 +89,32 @@ class Line(pygame.sprite.Sprite):
         self.rect.topleft = pos
         
 # Draws grid of 15 vertical lines x 9 horizontal lines
-class Grid(pygame.sprite.Group):
-    def __init__(self):
+class Gridlines(pygame.sprite.Group):
+    def __init__(self, xoffset=0, yoffset=0):
         pygame.sprite.Group.__init__(self)
-        for i in range(1,10):
-            self.add(Line((720,2),(0,45*i)))
-        for i in range(1,16):
-            self.add(Line((2,450),(45*i,0)))
+        for i in range(0,11):
+            self.add(Line((720,2),(xoffset,45*i-1+yoffset)))
+        for i in range(0,17):
+            self.add(Line((2,450),(45*i-1+xoffset,yoffset)))
 
 # The line that wipes out blocks
 class Wiper(pygame.sprite.GroupSingle):
-    def __init__(self, speed):
+    def __init__(self, speed, xoffset=0, yoffset=0):
         pygame.sprite.GroupSingle.__init__(self)
         self.sprite = pygame.sprite.Sprite()
         self.sprite.image = pygame.Surface((2,450))
         self.sprite.image.fill(yellow)
         self.sprite.rect = self.sprite.image.get_rect()
-        self.sprite.rect.topleft = (0,0)
+        self.xoffset = xoffset
+        self.sprite.rect.topleft = (xoffset,yoffset)
+        
         # Velocity is measured in pixels per second
         self.speed = speed
-        self.left_float = 0.0
+        self.left_float = 0.0+self.sprite.rect.left
 
     def update(self, time):
         self.left_float = self.left_float + self.speed*time
-        if (self.left_float > 748):
-            self.left_float = 0.0
+        if (self.left_float > 719+self.xoffset):
+            self.left_float = 0.0+self.xoffset
         self.sprite.rect.left = self.left_float
         
--- main.py
+++ main.py
@@ -9,15 +9,19 @@ red = (255,0,0)
 charcoal = (37,37,38)
 
 def main():
-    screendimensions = (720,450)
+    screendimensions = (800,500)
+    griddimensions = (720,450)
     pygame.init()
     screen = pygame.display.set_mode(screendimensions)
     clock = pygame.time.Clock()
     pygame.mouse.set_visible(True)
 
-    tile = blocks.Tile(red,white)
-    grid = blocks.Grid()
-    wiper = blocks.Wiper(150)
+    color1 = red
+    color2 = white
+    manager = blocks.BlockManager(red,white,150,0.75,
+                                  (screendimensions[0]-griddimensions[0])/2,
+                                  screendimensions[1]-griddimensions[1]-2)
+    tile = blocks.Tile(color1,color2)
 
     while 1:
         time = clock.tick(60)/1000
@@ -25,11 +29,9 @@ def main():
             if event.type == QUIT:
                 return
 
-        wiper.update(time)
+        manager.update(time)
         screen.fill(charcoal)
-        grid.draw(screen)
-        screen.blit(tile.image,pygame.mouse.get_pos())
-        wiper.draw(screen)
+        manager.draw(screen)
         pygame.display.flip()
 
 if __name__ == '__main__':
